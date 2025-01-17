ac7e72a02b338b142bdb88c463bff757af3d5e97
skyl3-r <skyler.ng.nyz@gmail.com>
2025-01-17 13:35:15 +0800

    fix build error

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -7,6 +7,7 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData, fetchFullMeetings } f
 import { Metadata } from 'next';
 import Link from 'next/link';
 import { PlusIcon } from '@heroicons/react/24/outline';
+import { CreateMeeting } from '@/app/ui/invoices/buttons';
 
 export const metadata: Metadata = {
     title: 'Dashboard',
@@ -43,17 +44,5 @@ export default async function Page() {
       <CreateMeeting />
       </div>
     </main>
-  );
-}
-
-export function CreateMeeting() {
-  return (
-    <Link
-      href="/dashboard/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-    >
-      <span className="">Create Meeting</span>{' '}
-      <PlusIcon className="h-5 md:ml-4" />
-    </Link>
   );
 }
